0f51b224944a62c2058c100419910dc181fd76ff
Carl Woodfin <carlwoodfin@users.noreply.github.com>
2015-07-15 16:52:04 +0100

    added loop to index.php

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
       <?php get_header(); ?>
   
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    
+
+    <?php endwhile; else: ?>
+      <p><?php _e('Sorry, there are no posts.'); ?></p>
+    <?php endif; ?>
+
+  </div>
 
       <?php get_footer(); ?>
