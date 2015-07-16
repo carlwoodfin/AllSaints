6326b6c299b6c80034335e76f668f93f1e7fb8ac
Carl Woodfin <carlwoodfin@users.noreply.github.com>
2015-07-16 16:32:50 +0100

    changed code tags

--- home.php
+++ home.php
@@ -1,13 +1,19 @@
 <?php
 
 get_header();
+
+?>
+
+<?php
  
 // Get the last 10 posts in the special_cat category.
  query_posts( 'cat=4&posts_per_page=1' );
 
  while ( have_posts() ) : the_post();
+?>
 	<h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
- endwhile; 
- 
+ <?php endwhile; ?>
+
+<?php 
 get_footer();
 ?>
