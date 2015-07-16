cc4ba23d4b7289ee33fe9cefcb250e2db86deada
Carl Woodfin <carlwoodfin@users.noreply.github.com>
2015-07-16 15:51:38 +0100

    removed additional php tags

--- home.php
+++ home.php
@@ -3,11 +3,11 @@
 get_header();
  
 // Get the last 10 posts in the special_cat category.
-<?php query_posts( 'cat=3&posts_per_page=1' ); ?>
+ query_posts( 'cat=3&posts_per_page=1' );
 
-<?php while ( have_posts() ) : the_post(); ?>
+ while ( have_posts() ) : the_post();
 	<!-- Do special_cat stuff... -->
-<?php endwhile; ?>
+ endwhile; 
  
 get_footer();
 ?>
