71d7c4a87aac67916deffffa05f61e3abfabb329
Carl Woodfin <carlwoodfin@users.noreply.github.com>
2015-07-16 16:47:19 +0100

    added content

--- home.php
+++ home.php
@@ -12,6 +12,10 @@ get_header();
  while ( have_posts() ) : the_post();
 ?>
 	<h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+	
+	<p class=""><?php the_content(); ?></p>
+	
+	
  <?php endwhile; ?>
 
 <?php 
