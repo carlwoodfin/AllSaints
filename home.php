0502efb9a752684983abb02f829ebd33c92e66fe
Carl Woodfin <carlwoodfin@users.noreply.github.com>
2015-07-16 16:15:46 +0100

    changed category

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
 get_header();
  
 // Get the last 10 posts in the special_cat category.
- query_posts( 'cat=3&posts_per_page=1' );
+ query_posts( 'cat=4&posts_per_page=1' );
 
  while ( have_posts() ) : the_post();
 	//<!-- Do special_cat stuff... -->
