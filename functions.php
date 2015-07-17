634ff5d10f4ecde18e02edc4e0718df5fac187a4
Carl Woodfin <carlwoodfin@users.noreply.github.com>
2015-07-17 15:53:12 +0100

    sidebar

--- functions.php
+++ functions.php
@@ -22,5 +22,27 @@ add_action( 'after_setup_theme', 'wpt_setup' );
             register_nav_menu( 'primary', __( 'Primary navigation', 'wptuts' ) );
         } endif;
 
+/* Sidebar */
+if ( function_exists('register_sidebar') ) {
+register_sidebar(array(
+'before_widget' => '<li id="%1$s" class="widget %2$s">',
+'after_widget' => '</li>',
+'before_title' => '<h2 class="widgettitle">',
+'after_title' => '</h2>',
+));
+}
+
+if ( function_exists('register_sidebar') ) {
+register_sidebar(array(
+'name' => 'Homepage Sidebar',
+'id' => 'homepage-sidebar',
+'description' => 'Appears as the sidebar on the custom homepage',
+'before_widget' => '<div style="height: 280px"></div><li id="%1$s" class="widget %2$s">',
+'after_widget' => '</li>',
+'before_title' => '<h2 class="widgettitle">',
+'after_title' => '</h2>',
+));
+}
+
 
 ?>
--- sidebar.php
+++ sidebar.php
@@ -5,23 +5,10 @@
 
 ?>
 
-<?php
-       /*
-         * When we call the dynamic_sidebar() function, it'll spit out
-         * the widgets for that widget area. If it instead returns false,
-         * then the sidebar simply doesn't exist, so we'll hard-code in
-         * some default sidebar stuff just in case.
-        */
-        if ( ! dynamic_sidebar( 'primary-widget-area' ) ) : ?>
-?>
-<?php
-        // A second sidebar for widgets, just because.
-      if ( is_active_sidebar( 'secondary-widget-area' ) ) : ?>
-      	
-                <div id="secondary" class="widget-area" role="complementary">
-                        <ul class="xoxo">
-                             <?php dynamic_sidebar( 'secondary-widget-area' ); ?>
-                        </ul>
-                </div><!-- #secondary .widget-area -->
-
-<?php endif; ?>
+<div id="sidebar">
+   <ul>
+      <?php
+      if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('sidebar') ) :
+      endif; ?>
+   </ul>
+</div>
