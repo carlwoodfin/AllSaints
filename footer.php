a1e30548f093b22dee6be5f2e8a9fcc05762cb49
Carl Woodfin <carlwoodfin@users.noreply.github.com>
2015-07-21 09:02:35 +0100

    updated footer

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 		<?php get_sidebar(); ?>
 </div>
 
-</div> <!-- The container ending -->
+
 </div> <!-- #wrap ending -->
 <footer>
 		<div class="footer col-md-12">
@@ -11,6 +11,7 @@
       		<p>&copy; <?php echo date("Y"); echo " "; bloginfo('name'); ?>. Built with <a href="http://wordpress.org/">WordPress</a>, <a href="http://getbootstrap.com/">Bootstrap</a></p>
 		<div>
       </footer>
+</div> <!-- The container ending -->
 
     
 	<?php wp_footer(); ?>
