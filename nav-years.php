0cbaa0cedb068ca0189cc86a7ceb0bf7135460c9
Carl Woodfin <carlwoodfin@users.noreply.github.com>
2015-07-26 18:40:46 +0100

    nav year buttons

--- nav-years.php
+++ nav-years.php
@@ -2,7 +2,7 @@
     <!-- Brand and toggle get grouped for better mobile display -->
    
 
-    <div class="" id="bs-example-navbar-collapse-2">
+    
     <?php /* Years Navigation */
       wp_nav_menu( array(
       'menu' => 'Years',
@@ -14,8 +14,6 @@
       'walker' => new wp_bootstrap_navwalker())
       );
     ?>
-      
-    </div><!-- /.navbar-collapse -->
-    </nav>
+    
     </div>
     <hr>
