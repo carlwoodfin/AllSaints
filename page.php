1ede565ed339addde7918d0fbae821503639b668
Carl Woodfin <carl.woodfin@gmail.com>
2015-07-27 02:32:55 +0100

    comment styles

--- page.php
+++ page.php
@@ -1,18 +1,22 @@
 <?php get_header(); ?>
 
 <div class="row">
-  <div class="col-md-9">
+  <div class="col-md-8">
 
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 		<h3><?php the_title(); ?></h3>
+	  	
 	  	<?php the_content(); ?>
+
+	  	<hr>
+		<?php comments_template(); ?
 
 	<?php endwhile; else: ?>
 		<p><?php _e('Sorry, this page does not exist.'); ?></p>
 	<?php endif; ?>
 
   </div>
-  <div class="col-md-3">
+  <div class="col-md-4">
 	<?php get_sidebar(); ?>	
   </div>
 </div>
