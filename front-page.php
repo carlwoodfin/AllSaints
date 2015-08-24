8123fe5f049e897116f3daffed41f87de2a85b93
Carl Woodfin <carl.woodfin@gmail.com>
2015-08-25 00:21:06 +0100

    Messed with styles and colours

--- front-page.php
+++ front-page.php
@@ -3,7 +3,7 @@
 get_header();
 
 ?>
-  <div class="row">
+<div class="row">
  <div class="col-md-10 col-md-offset-1">
 
 <div id="postCarousel" class="carousel slide" data-ride="carousel">
@@ -48,35 +48,28 @@ get_header();
 </div> <!-- Carousel row ending -->
 <hr>
 <div class="row">
+<div class="col-md-6 col-md-offset-3">
+
+
+</div>
+</div>
+<hr>
+
+
+
+<div class="col-md-6 col-md-offset-3 text-center">
+<h5 class="year-work">See some of our children's work!</h5>
+<?php get_template_part( 'nav', 'years' ); ?>
+</div>
+
+<div class="row">
 <div class="col-md-9">
-<?php
- 
-// Get the last 10 posts in the special_cat category.
- query_posts( 'pagename=Home' );
-
- while ( have_posts() ) : the_post();
-?>
-	<h3><?php the_title(); ?></h3>
-	
-	<p class="homecontent"><?php the_content(); ?></p>
-	
-
- <?php endwhile; ?>
-</div>  
-
-
-
-
+</div>
   <div class="col-md-3">
 <?php get_sidebar(); ?>
 </div>
+</div>
 
-<div class="row text-center year-work">
-<h5 class="">See some of our children's work!</h5>
-<div class="col-md-12">
-<?php get_template_part( 'nav', 'years' ); ?>
-</div>
-</div>
-</div> <!-- Row ending -->
+ <!-- Row ending -->
 
 <?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -2,15 +2,17 @@
 
 
 /* Register Navigation Helper */
-require_once('wp_bootstrap_navwalker.php');
+require_once('includes/wp_bootstrap_navwalker.php');
 
 /* Register Bootsrap + JQuery - Add any other needed scripts here */
 function reg_scripts() {
 	wp_enqueue_script( 'jquery' );
     wp_enqueue_style( 'bootstrapstyle', get_template_directory_uri() . '/bootstrap/css/bootstrap.min.css' );
     wp_enqueue_style( 'bootstrapthemestyle', get_template_directory_uri() . '/bootstrap/css/bootstrap-theme.min.css' );
+    wp_enqueue_style( 'fontawesomecss', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css' );
     wp_enqueue_style( 'stylesheetcss', get_template_directory_uri() . '/style.css' );
     wp_enqueue_script( 'bootstrap-script', get_template_directory_uri() . '/bootstrap/js/bootstrap.min.js', array(), true );
+    wp_enqueue_script( 'bootstrapwpjs', get_template_directory_uri() . '/includes/bootstrap-wp.js', array('jquery') );
     
 }
 add_action('wp_enqueue_scripts', 'reg_scripts');
@@ -56,28 +58,10 @@ if (function_exists('register_sidebar')) {
 		'id'   => 'header',
 		'description'   => 'This is the widgetized header.',
 		'class'			=> '',
-		'before_widget' => '<div class="panel panel-primary widget %2$s">',
-		'after_widget'  => '</div></div>',
-		'before_title'  => '<div class="panel-heading"><h3 class="panel-title">',
-		'after_title'   => '</h3></div><div class="panel-body">'
-	));
-	register_sidebar(array(
-		'name' => 'Sidebar',
-		'id'   => 'sidebar',
-		'description'   => 'This is the widgetized sidebar.',
-		'before_widget' => '<div class="widget %2$s">',
-		'after_widget'  => '</div>',
-		'before_title'  => '<h3>',
-		'after_title'   => '</h3>'
-	));
-	register_sidebar(array(
-		'name' => 'Footer',
-		'id'   => 'footer',
-		'description'   => 'This is the widgetized footer.',
-		'before_widget' => '<div id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</div>',
-		'before_title'  => '<h3>',
-		'after_title'   => '</h3>'
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</aside>',
+        'before_title'  => '<h3 class="widget-title">',
+        'after_title'   => '</h3>',
 	));
 
 }
