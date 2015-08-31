ca25f743e3f36d07d3ce190c8078cba7891d5e2e
Carl Woodfin <carl.woodfin@gmail.com>
2015-08-31 23:24:53 +0100

    fixed footer width

--- includes/content-footer.php
+++ includes/content-footer.php
@@ -2,15 +2,15 @@
   	<hr>
   		<div class="footer">
   			<div class="col-md-12 text-center">
-  			<div class="footer-address">
-				<p>Church St, Heaton Norris, Stockport, SK4 1ND<br>
-				Tel: 0161 285 7373 | Fax: 0161 218 1188</p>
+  				<div class="footer-address">
+					<p>Church St, Heaton Norris, Stockport, SK4 1ND<br>
+					Tel: 0161 285 7373 | Fax: 0161 218 1188</p>
 				</div>
 			</div>
 		</div>
 		<p class="pull-right">&copy; <?php echo date("Y"); echo " "; bloginfo('name'); ?>  </p>
 	<?php wp_footer(); ?>
 	</div>
-</diV> <!-- Container Ending -->
+	</div>
   </body>
 </html>
--- includes/nav-years.php
+++ includes/nav-years.php
@@ -1,24 +1,19 @@
-<div class="row">
-<div class="text-center">
+<div class="row text-center">
     <a href="<?php echo get_post_type_archive_link( 'year-1' ); ?>">
     <button type="button"  class="btn btn-glyph btn-circle btn-l">1</i></button>
     </a>
-  
-   
+
     <a href="<?php echo get_post_type_archive_link( 'year-2' ); ?>">
     <button type="button"  class="btn btn-glyph btn-circle btn-l">2</i></button>
     </a>
-  
- 
+
     <a href="<?php echo get_post_type_archive_link( 'year-3' ); ?>">
     <button type="button"  class="btn btn-glyph btn-circle btn-l">3</i></button>
     </a>
- 
 
     <a href="<?php echo get_post_type_archive_link( 'year-4' ); ?>">
     <button type="button"  class="btn btn-glyph btn-circle btn-l">4</i></button>
     </a>
-
 
     <a href="<?php echo get_post_type_archive_link( 'year-5' ); ?>">
     <button type="button"  class="btn btn-glyph btn-circle btn-l">5</i></button>
@@ -28,6 +23,5 @@
     <button type="button"  class="btn btn-glyph btn-circle btn-l">6</i></button>
     </a>
 
-</div>
 <hr>
 </div>
